41d1372249d814bacbaec049ca2598bb9134657d
leonardo rocha <leonardo.rocha1990@gmail.com>
2021-10-19 22:37:19 -0500

    updating jenkins pipeline

--- src/integration/java/com/leonardo/rocha/wedding/controller/RsvpControllerTest.java
+++ src/integration/java/com/leonardo/rocha/wedding/controller/RsvpControllerTest.java
@@ -20,8 +20,15 @@ import java.util.Objects;
 public class RsvpControllerTest {
     private static final Logger logger = LoggerFactory.getLogger(RsvpControllerTest.class);
 
-    @Autowired
     RsvpController cut;
+
+    @Autowired
+    public RsvpControllerTest(RsvpController cut) {
+        logger.info("look leo SPRING_PROFILES_ACTIVE: {}", System.getenv("SPRING_PROFILES_ACTIVE"));
+        logger.info("look leo SPRING_DATASOURCE_PASSWORD: {}", System.getenv("SPRING_DATASOURCE_PASSWORD"));
+
+        this.cut = cut;
+    }
 
     @Before
     public void setUp() throws Exception {
