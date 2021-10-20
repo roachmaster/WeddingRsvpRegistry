50487cfaac09829370c2796222f44f3d1906f1f0
Leonardo <leonardo.rocha1990@gmail.com>
2021-10-20 17:29:18 -0500

    update to guest

--- src/main/java/com/leonardo/rocha/wedding/data/Guest.java
+++ src/main/java/com/leonardo/rocha/wedding/data/Guest.java
@@ -16,8 +16,10 @@ public class User {
 
     private String name;
     private int age;
+
     public User() {
     }
+
     public User(String name, int age) {
         this.name = name;
         this.age = age;
