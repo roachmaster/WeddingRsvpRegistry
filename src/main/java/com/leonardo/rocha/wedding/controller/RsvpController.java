f5987e9a95d1f84b59776086d087227139bcad28
Leonardo <leonardo.rocha1990@gmail.com>
2021-11-22 15:42:18 -0600

    updating contoller test

--- src/main/java/com/leonardo/rocha/wedding/controller/RsvpController.java
+++ src/main/java/com/leonardo/rocha/wedding/controller/RsvpController.java
@@ -4,6 +4,8 @@ import com.leonardo.rocha.wedding.data.DeleteAllResponse;
 import com.leonardo.rocha.wedding.data.Guest;
 import com.leonardo.rocha.wedding.helper.ResponseEntityHelper;
 import com.leonardo.rocha.wedding.service.GuestDB;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -14,6 +16,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/")
 public class RsvpController {
+	private static final Logger logger = LoggerFactory.getLogger(RsvpController.class);
 
 	private final GuestDB guestDao;
 
@@ -24,26 +27,31 @@ public class RsvpController {
 
 	@RequestMapping(value = "guests", method = RequestMethod.GET)
 	public ResponseEntity<List<Guest>> getGuests() {
+		logger.info("Getting Guests");
 		return ResponseEntityHelper.getGuests(this.guestDao);
 	}
 
 	@RequestMapping(value = "guest/{name}", method = RequestMethod.GET)
 	public ResponseEntity<Guest> getGuest(@PathVariable String name) {
+		logger.info("Getting Guest {}", name);
 		return ResponseEntityHelper.getGuest(this.guestDao,name);
 	}
 
 	@RequestMapping(value = "guest/create/name/{name}/maxGuest/{maxGuest}", method = RequestMethod.POST)
 	public ResponseEntity<Guest> createGuest(@PathVariable String name, @PathVariable int maxGuest) {
+		logger.info("Creating Guest name: {}, maxGuest: {}", name, maxGuest);
 		return ResponseEntityHelper.createGuest(this.guestDao, name, maxGuest);
 	}
 
 	@RequestMapping(value = "guests/delete", method = RequestMethod.GET)
 	public ResponseEntity<DeleteAllResponse> deleteGuests() {
+		logger.info("Deleting Guests");
 		return ResponseEntityHelper.deleteGuests(this.guestDao);
 	}
 
 	@RequestMapping(value = "guest/update/name/{name}/going/{going}/confirmedGuest/{confirmedGuest}", method = RequestMethod.POST)
 	public ResponseEntity<Guest> updateGuest(@PathVariable String name, @PathVariable boolean going ,@PathVariable int confirmedGuest) {
+		logger.info("Updating Guest name: {}, going: {}, confirmedGuest: {}", name, going, confirmedGuest);
 		return ResponseEntityHelper.updateGuest(this.guestDao,name,going, confirmedGuest);
 	}
 }
